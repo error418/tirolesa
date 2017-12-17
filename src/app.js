4945dbd406d727d58e44ba0858e2abb5a7b350fe
error418 <error418@users.noreply.github.com>
2017-12-17 20:19:01 +0100

    rename sources

--- src/app.js
+++ src/app.js
@@ -4,15 +4,15 @@ angular.module("Thelemic", ["ngResource", "ngRoute", "ngAnimate", "ui.bootstrap"
     function($routeProvider, $locationProvider) {
         $routeProvider
             .when('/', {
-            templateUrl: 'start/start.html',
-            controller: 'StartController'
+            templateUrl: 'org-select/org-select.html',
+            controller: 'OrgSelectController'
         })
         .when('/org-select', {
             templateUrl: 'organization/org-select.html',
             controller: 'OrganizationController'
         })
         .when('/org/:orgName', {
-            templateUrl: 'repo-template/configuration.html',
+            templateUrl: 'repo-template/repo-template.html',
             controller: 'RepoTemplateController'
         })
         .when('/create', {
--- src/org-select/org-select.ctrl.js
+++ src/org-select/org-select.ctrl.js
@@ -1,6 +1,6 @@
 angular.module("Thelemic")
 
-.controller("StartController", function($scope, AuthService, OrganizationService) {
+.controller("OrgSelectController", function($scope, AuthService, OrganizationService) {
     $scope.auth = AuthService.info(
         function(success) {
             $scope.orgs = OrganizationService.get();
