0e351b61e73c0d1e225e0ebf693e119dd0c855ee
error418 <error418@users.noreply.github.com>
2018-02-22 21:03:28 +0100

    feat: config via env vars for session storage

--- server/config.js
+++ server/config.js
@@ -10,4 +10,13 @@ config.github.base = process.env.GITHUB_BASE || config.github.base
 config.application.port = process.env.APP_PORT || config.application.port
 config.session.secret = process.env.SESSION_SECRET || config.session.secret
 
+if (process.env.REDIS_HOST) {
+    config.session.redis = {
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT,
+        ttl: process.env.REDIS_TTL,
+        logErrors: process.env.REDIS_LOG_ERRORS
+    }
+}
+
 module.exports = config;
